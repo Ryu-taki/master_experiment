7219e96f806c9b3f24a0bacbc8ac187ab23df161
Ryu-taki <takiryu0616@gmail.com>
2025-07-08 12:01:40 +0900

    implemented exercise3

---
+++ exercise3.py
@@ -0,0 +1,58 @@
+import os
+import pickle
+
+import numpy as np
+
+from common_func import (
+    load_values_from_pickle,
+)
+
+
+def calc_utilitiy(
+    b: float,
+    phi: float,
+    s_values: np.ndarray,
+    i_values: np.ndarray,
+    cs: float,
+    ci: float,
+    cd: float,
+) -> float:
+    uv: float = -(ci + phi * cd) * i_values[-1]
+    ub: float = np.sum(-cs * (b - 1.0) ** 2 * s_values - (ci - phi * cd) * i_values)
+
+    return uv + ub
+
+
+if __name__ == "__main__":
+    # --- Define parameters ---
+    phi: float = 0.029
+    cs: float = 1.0
+    ci: float = 3.0
+    cd: float = 1000.0
+
+    # --- Calculate utilities for four scenarios ---
+    utility_values: dict[str, float] = {}
+    for b in [10, 8, 6, 5]:
+        # Load the simulation values for the current b
+        if b == 10:
+            value_dict = load_values_from_pickle(
+                "./output/exercise1/simulation_values/simulation_values.pkl"
+            )
+        else:
+            value_dict = load_values_from_pickle(
+                f"./output/exercise2/simulation_values/simulation_values_b{b}.pkl"
+            )
+        s_values: np.ndarray = value_dict["s_values"]
+        i_values: np.ndarray = value_dict["i_values"]
+
+        # Calculate utilities
+        utility_value: float = calc_utilitiy(
+            b / 10, phi, s_values, i_values, cs, ci, cd
+        )
+        utility_values[b] = utility_value
+        print(f"Utility value (b={b / 10}): {utility_value}")
+
+    # --- Save utility values as a pickle file ---
+    os.makedirs("./output/exercise3", exist_ok=True)
+    with open("./output/exercise3/utility_values.pkl", "wb") as f:
+        pickle.dump(utility_values, f)
